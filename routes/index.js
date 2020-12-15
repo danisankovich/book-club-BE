6ae9409235a7aa763ee1c000c010438a3ad26962
Dani Sankovich <misankovich@gmail.com>
2020-12-15 11:51:17 -0500

    router placeholders

--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,24 @@ router.get('/search', async function(req, res) {
     });
   }).on('error', (e) => {
     console.error(e);
-  })
+  });
 });
+router.post('/pitch', async function(req, res) {
+  console.log(req.body);
+  res.send();
+});
+
+router.get('/poll', async function(req, res) {
+  res.send([
+    {id: '12345', title: 'title one', authors: 'name here', thumbnail: 'http://books.google.com/books/content?id=HHJwDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api', description: 'just a description'},
+    {id: '67890', title: 'title two', authors: 'name here', thumbnail: 'http://books.google.com/books/content?id=HHJwDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api', description: 'just a description'},
+    {id: 'abcde', title: 'title three', authors: 'name here', thumbnail: 'http://books.google.com/books/content?id=HHJwDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api', description: 'just a description'},
+  ]);
+});
+
+router.post('/vote', async function(req, res) {
+  console.log(req.body);
+  res.send()
+})
 
 module.exports = router;
